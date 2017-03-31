6eb18d8617c3712b6f0d097312df3a40a15d408b
Brandon Wulf <mrwulf@gmail.com>
2017-03-31 15:50:33 -0700

    Try some explicit logging

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-LOG_LEVEL="ERR"
+LOG_LEVEL="DEBUG"
 
 DATADIR_DC1=$(mktemp -d /tmp/consul-test1.XXXXXXXXXX)
 DATADIR_DC2=$(mktemp -d /tmp/consul-test2.XXXXXXXXXX)
@@ -109,7 +109,7 @@ curl -sLo /dev/null -X PUT $ADDRESS_DC1/v1/kv/global/$EXCLUDED_KEY -d "test data
 sleep 3
 
 echo "    Checking key still exists in DC2"
-curl -sL $ADDRESS_DC2/v1/kv/backup/$EXCLUDED_KEY/nodelete | grep -q "ZG9uJ3QgZGVsZXRl" || (curl -sL $ADDRESS_DC2/v1/kv/backup/$EXCLUDED_KEY/nodelete && exit 1)
+curl -sL $ADDRESS_DC2/v1/kv/backup/$EXCLUDED_KEY/nodelete | grep -q "ZG9uJ3QgZGVsZXRl"
 
 echo "##Test Case #2"
 echo "    Writing a key in DC2"
@@ -121,7 +121,7 @@ curl -sLo /dev/null -X PUT $ADDRESS_DC1/v1/kv/global/parent_folder/other_folder/
 sleep 3
 
 echo "    Checking key still exists in DC2"
-curl -sL $ADDRESS_DC2/v1/kv/backup/parent_folder/test_folder/testkey | grep -q "ZG9uJ3QgZGVsZXRl" || (curl -sL $ADDRESS_DC2/v1/kv/backup/parent_folder/test_folder/testkey && exit 1)
+curl -sL $ADDRESS_DC2/v1/kv/backup/parent_folder/test_folder/testkey | grep -q "ZG9uJ3QgZGVsZXRl"
 
 rm -rf $DATADIR_DC1
 rm -rf $DATADIR_DC2
